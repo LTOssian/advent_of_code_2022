e800d17e6c533d6cdbed05c40cf15c3e2ebc853f
Louisan TCHITOULA <t.louisan@outlook.fr>
2022-12-11 23:16:06 +0100

    answered the second part of the problem

--- day1/calorie_counting.py
+++ day1/calorie_counting.py
@@ -26,11 +26,26 @@ elfs_inventories = sort_input(file)
 
 def get_highest_calories(inventories):
     highest_calories = 0
+    index = 0
     for inventory in inventories:
         current_calories = sum(inventory)
         if current_calories > highest_calories:
             highest_calories = current_calories
-    return highest_calories
+            index = inventories.index(inventory)
+    return highest_calories, index
 
-answer = get_highest_calories(elfs_inventories)
-print(answer)
+top_carrying_elf, index_of_highest = get_highest_calories(elfs_inventories)
+print(top_carrying_elf, index_of_highest) #first_answer
+
+"""
+Answer to the second part will be the sum of the 3 highest calories carried by elves
+"""
+# uses list comprehension to create a new lists without the previous highest carrying elf 
+
+second_list = [x for x in elfs_inventories if elfs_inventories.index(x) != index_of_highest]
+second_carrrying_elf, index_of_second = get_highest_calories(second_list) 
+
+third_list = [x for x in second_list if second_list.index(x) != index_of_second]
+third_carrying_elf, index_of_third = get_highest_calories(third_list) 
+
+print(top_carrying_elf + second_carrrying_elf + third_carrying_elf) #second_answer
