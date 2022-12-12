cec604db0e702bcd9f250843c3118ce28093acb3
Louisan TCHITOULA <t.louisan@outlook.fr>
2022-12-12 01:27:00 +0100

    answered first part of the problem

---
+++ day2/rock_paper_scissors.py
@@ -0,0 +1,60 @@
+"""
+Puzzle answer if the total score if a rps game
+"""
+file = 'input.txt'
+
+#dict to identify the keyword corresponding to the input
+rules = {
+    'rock':  ['A', 'X'],
+    'paper': ['B', 'Y'],
+    'scissors': ['C','Z']
+}
+#dict to identify the wkeaness of the key
+win_conditions = {
+    'rock': 'scissors',
+    'paper': 'rock',
+    'scissors': 'paper'
+}
+
+#dict to identify the points given for each keyword
+keywords_to_points = {
+    'rock': 1,
+    'paper': 2,
+    'scissors': 3
+}
+
+#sort the input, separate each games and each opponents inputs
+def sort_input(file):
+    with open (file) as file:
+            inputs = file.read()
+
+    return [x.split() for x in inputs.split('\n')]
+
+strategy_guide = sort_input(file)
+
+#checks if the key value corresponds to a win or a loss, else its a draw -> returns the score according to the result
+def score_checker(opponent_choice, elf_choice):
+    keyword_points = keywords_to_points[elf_choice]
+    if win_conditions[opponent_choice] == elf_choice:
+        return 0 + keyword_points
+    elif win_conditions[elf_choice] == opponent_choice:
+        return 6 + keyword_points
+    else:
+        return 3 + keyword_points
+
+#sum the total score for every game
+def total_points(games_history):
+    score = 0
+    for game in games_history:
+        opponent_choice, elf_choice = game
+        #this loop swaps the letter for the keyword to access the dicts in score_checker()
+        for key, value in rules.items():
+            if opponent_choice in value:
+                opponent_choice = key
+            if elf_choice in value:
+                elf_choice = key
+
+        score += score_checker(opponent_choice, elf_choice)
+    return score
+
+print(total_points(strategy_guide)) #first_answer
